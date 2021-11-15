4959f83dccf0d8b9f75beeb21a0ea934a7eaea6c
dnflekals <dnflekals@gmail.com>
2021-11-15 15:49:55 +0900

    feat: 한칸 이동할지 말지 결정하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
 
     private final String name;
@@ -10,6 +12,19 @@ public class Car {
     }
 
     void move() {
-        
+        boolean goOneStep = canMove();
+        if (goOneStep) {
+            position++;
+        }
+    }
+
+    private boolean canMove() {
+        int randomNumber = RandomUtils.nextInt(0, 9);
+
+        if (randomNumber > 3) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
