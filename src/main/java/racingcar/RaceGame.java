ece2b43741513bef14948626050ae54843345420
dnflekals <dnflekals@gmail.com>
2021-11-15 14:33:33 +0900

    feat: 입력 받은 문자열 split하는 기능 구현

--- src/main/java/racingcar/RaceGame.java
+++ src/main/java/racingcar/RaceGame.java
@@ -1,16 +1,26 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class RaceGame {
+
     private String inputString;
+    private List<String> carNames;
 
     private void inputCarName() {
         Scanner scanner = new Scanner(System.in);
         inputString = scanner.nextLine();
     }
 
+    private void splitInputString() {
+        carNames = new ArrayList<String>(Arrays.asList(inputString.split(",")));
+    }
+
     void startGame() {
         inputCarName();
+        splitInputString();
     }
 }
